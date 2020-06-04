e13a5ed3e813296e0c65b0f260b361a163f6abdd
noyfruman <noyfruman48@gmail.com>
2020-06-04 13:25:51 +0300

    Deploy to Netlify

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -12,6 +12,14 @@ import {
 
 let githubClientId;
 let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 const GithubState = (props) => {
   const initialState = {
     users: [],
